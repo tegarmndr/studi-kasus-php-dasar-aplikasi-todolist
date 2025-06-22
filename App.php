4583a8ac3e0d2a6326b323b141d6778cabf1eb46
Tegar Mandiri <27650396+tegarmndr@users.noreply.github.com>
2025-06-22 13:01:49 +0700

    feat(app): tambahkan require_once untuk show, add, dan remove todolist

--- App.php
+++ App.php
@@ -1,5 +1,8 @@
 <?php
 
 require_once "Model/Todolist.php";
+require_once "BusinessLogic/ShowTodoList.php";
+require_once "BusinessLogic/AddTodoList.php";
+require_once "BusinessLogic/RemoveTodoList.php";
 
 echo "Aplikasi Todolist" . PHP_EOL;
