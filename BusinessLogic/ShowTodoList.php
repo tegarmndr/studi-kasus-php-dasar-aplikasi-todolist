0ae38fafb3706d4fcd47cd9dbe6f2b19cc070bb0
Tegar Mandiri <27650396+tegarmndr@users.noreply.github.com>
2025-06-22 17:50:25 +0700

    feat(logic): implementasikan fungsi showTodoList untuk menampilkan todo

--- BusinessLogic/ShowTodoList.php
+++ BusinessLogic/ShowTodoList.php
@@ -4,7 +4,14 @@
  * Menampilkan todo di list
  * @return void
  */
-function showTodoList()
+function showTodoList() : void
 {
+    global $todoList;
 
+    echo "TODOLIST" . PHP_EOL;
+
+    foreach ($todoList as $index => $value)
+    {
+        echo "$index. $value" . PHP_EOL;
+    }
 }
