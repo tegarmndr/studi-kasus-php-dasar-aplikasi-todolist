f76fa26903e5bf1dc917c7471f64589e775b5310
Tegar Mandiri <27650396+tegarmndr@users.noreply.github.com>
2025-06-27 20:17:36 +0700

    feat(logic): implementasikan fungsi removeTodoList untuk menghapus todo

--- BusinessLogic/RemoveTodoList.php
+++ BusinessLogic/RemoveTodoList.php
@@ -2,9 +2,25 @@
 
 /**
  * Menghapus todo di list
- * @return void
+ *
+ * @param int $number Nomor item yang dihapus dari todoList
+ * @return bool
  */
-function removeTodoList()
+function removeTodoList(int $number): bool
 {
+    global $todoList;
 
+    if ($number > sizeof($todoList)) {
+        return false;
+    } else if ($number === 0) {
+        return false;
+    }
+
+    for ($i = $number; $i < sizeof($todoList); $i++) {
+        $todoList[$i] = $todoList[$i+1];
+    }
+
+    unset($todoList[sizeof($todoList)]);
+
+    return true;
 }
