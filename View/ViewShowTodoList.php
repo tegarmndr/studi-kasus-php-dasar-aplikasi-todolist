da4b07cc66aa954b7e357109511c65818dd66208
Tegar Mandiri <27650396+tegarmndr@users.noreply.github.com>
2025-06-27 20:44:45 +0700

    feat(view): implementasikan tampilan pilihan menu todolist

--- View/ViewShowTodoList.php
+++ View/ViewShowTodoList.php
@@ -1,6 +1,33 @@
 <?php
 
+require_once "../Model/TodoList.php";
+require_once "../BusinessLogic/ShowTodoList.php";
+require_once "../View/ViewAddTodoList.php";
+require_once "../View/ViewRemoveTodoList.php";
+require_once "../Helper/Input.php";
+
 function viewShowTodoList()
 {
+    while (true) {
+        showTodoList();
 
+        echo "MENU" . PHP_EOL;
+        echo "1. Tambah Todo" . PHP_EOL;
+        echo "2. Hapus Todo" . PHP_EOL;
+        echo "x. Keluar" . PHP_EOL;
+
+        $pilihan = input("Pilih");
+
+        if ($pilihan === "1") {
+            viewAddTodoList();
+        } else if ($pilihan === "2") {
+            viewRemoveTodoList();
+        } else if ($pilihan === "x") {
+            break;
+        } else {
+            echo "Pilihan tidak dimengerti!" . PHP_EOL;
+        }
+    }
+
+    echo "Sampai jumpa lagi!" . PHP_EOL;
 }
