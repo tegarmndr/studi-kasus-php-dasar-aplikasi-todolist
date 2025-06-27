7d86e2cb6fa77e8adea3c74ffed18ec1ec3234ce
Tegar Mandiri <27650396+tegarmndr@users.noreply.github.com>
2025-06-27 19:53:59 +0700

    feat(logic): implementasikan fungsi addTodoList untuk menambahkan todo

--- BusinessLogic/AddTodoList.php
+++ BusinessLogic/AddTodoList.php
@@ -2,9 +2,15 @@
 
 /**
  * Menambah todo ke list
+ *
+ * @param string $todo Item yang ditambahkan ke todoList
  * @return void
  */
-function addTodoList()
+function addTodoList(string $todo): void
 {
+    global $todoList;
 
+    $number = sizeof($todoList) + 1;
+
+    $todoList[$number] = $todo;
 }
